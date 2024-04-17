9926d2b57d9b10aff728095a6644801e32ce48ff
Sean Kelley <seannkelleyy1@gmail.com>
2024-04-17 16:15:29 -0400

    Revert "Merge branch 'main' of https://github.com/seannkelleyy/3241-Project"
    This reverts commit bbc1b5f144334d3b18541c95359b10cfb480fb27, reversing
    changes made to 1a2eb3a9a2f19755745e94f61cb533f4a0d8ed26.

---
+++ Bookstore.Data/DatabaseConnection.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Bookstore.Data
+{
+    public class DatabaseConnection
+    {
+        public static SqliteConnection conn;
+
+        public static void OpenConnection(string pathToDatabase)
+        {
+            conn = new SqliteConnection($"Data Source={pathToDatabase};");
+            conn.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            try
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                    conn.Dispose();
+                }
+            }
+            catch (Exception)
+            {
+                // Do nothing
+            }
+        }
+
+        public static void BeginTransaction()
+        {
+            using (SqliteCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "BEGIN TRANSACTION;";
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static void EndTransaction()
+        {
+            using (SqliteCommand cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "END TRANSACTION;";
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
